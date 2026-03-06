cf18dfd787e65c23d681365953f217c3f560b5ca
Vladimir Svyatski <svp@scand.com>
2026-03-06 11:11:26 +0300

    Getting rid of deprecated class

--- src/test/java/com/github/paulcwarren/ginkgo4j/spring/Ginkgo4jSpringApplicationTests.java
+++ src/test/java/com/github/paulcwarren/ginkgo4j/spring/Ginkgo4jSpringApplicationTests.java
@@ -5,15 +5,14 @@ import com.github.paulcwarren.ginkgo4j.spring.Ginkgo4jSpringApplication.HelloSer
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static com.github.paulcwarren.ginkgo4j.Ginkgo4jDSL.*;
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-@SuppressWarnings("deprecation")
 @RunWith(Ginkgo4jSpringRunner.class)
-@SpringApplicationConfiguration(classes = Ginkgo4jSpringApplication.class)
+@SpringBootTest(classes = Ginkgo4jSpringApplication.class)
 public class Ginkgo4jSpringApplicationTests {
 
     @Autowired

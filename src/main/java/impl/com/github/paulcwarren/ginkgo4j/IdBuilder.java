0607c9d4bf25008029c8314dd4ecb59117d80037
Vladimir Svyatski <v.svyatski@gmail.com>
2026-03-07 14:15:45 +0300

    Refactoring

--- src/main/java/impl/com/github/paulcwarren/ginkgo4j/IdBuilder.java
+++ src/main/java/impl/com/github/paulcwarren/ginkgo4j/IdBuilder.java
@@ -4,33 +4,34 @@ import java.util.Stack;
 
 public class IdBuilder {
 
-	private static final String EMPTY_ID = "_EMPTY_";
-	
-	private IdBuilder() {}
+    private static final String EMPTY_ID = "_EMPTY_";
 
-	public static String id(String id) {
-		if (id == null || id.trim().isEmpty()) {
-			id = EMPTY_ID; 
-		}
-		return id;
-	}
-	
-	public static String fqId(String id, Stack<?> context) {
-		if (id == null || id.trim().isEmpty()) {
-			id = EMPTY_ID; 
-		}
-		StringBuilder builder = new StringBuilder();
-		int i;
-		for (i=0; i < context.size(); i++) {
-			if (i > 0) {
-				builder.append('.');
-			}
-			builder.append(context.elementAt(i));
-		}
-		if (i > 0) {
-			builder.append('.');
-		}
-		builder.append(id);
-		return builder.toString();
-	}
+    private IdBuilder() {
+    }
+
+    public static String id(String id) {
+        if (id == null || id.isBlank()) {
+            id = EMPTY_ID;
+        }
+        return id;
+    }
+
+    public static String fqId(String id, Stack<?> context) {
+        if (id == null || id.isBlank()) {
+            id = EMPTY_ID;
+        }
+        StringBuilder builder = new StringBuilder();
+        int i;
+        for (i = 0; i < context.size(); i++) {
+            if (i > 0) {
+                builder.append('.');
+            }
+            builder.append(context.elementAt(i));
+        }
+        if (i > 0) {
+            builder.append('.');
+        }
+        builder.append(id);
+        return builder.toString();
+    }
 }
--- src/main/java/impl/com/github/paulcwarren/ginkgo4j/TitleBuilder.java
+++ src/main/java/impl/com/github/paulcwarren/ginkgo4j/TitleBuilder.java
@@ -2,14 +2,15 @@ package impl.com.github.paulcwarren.ginkgo4j;
 
 public class TitleBuilder {
 
-	private static final String EMPTY_TEXT = " ";
-	
-	private TitleBuilder() {}
-	
-	public static String title(String title) {
-		if (title == null || title.trim().isEmpty()) {
-			return EMPTY_TEXT; 
-		}
-		return title;
-	}
+    private static final String EMPTY_TEXT = " ";
+
+    private TitleBuilder() {
+    }
+
+    public static String title(String title) {
+        if (title == null || title.isBlank()) {
+            return EMPTY_TEXT;
+        }
+        return title;
+    }
 }
